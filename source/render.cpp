169300e77aa7a36314307e977f547482290a0dd8
Brandon Pickering <brandonpickering95@gmail.com>
2016-10-08 22:29:53 -0700

    Add shadow test (example 3)

--- source/render.cpp
+++ source/render.cpp
@@ -3,6 +3,25 @@
 #include "scene.hpp"
 
 
+struct intersection {
+  rtfloat dist;
+  scene_object *obj;
+};
+
+static intersection trace_ray(scene *s, ray3f ray) {
+  intersection result = { rtfloat_inf, nullptr };
+
+  for (scene_object *obj : s->objects) {
+    // TODO: Transformation computations
+    rtfloat t = obj->ray_test(ray);
+    if (t < result.dist)
+      result = { t, obj };
+  }
+
+  return result;
+};
+
+
 static color3f compute_shading(scene *s, scene_object *obj, vec3f point,
                                 vec3f eye) {
   color3f result = {0, 0, 0};
@@ -14,48 +33,46 @@ static color3f compute_shading(scene *s, scene_object *obj, vec3f point,
   vec3f normal = obj->get_normal(point);
 
   for (light_source light : s->lights) {
-    // TODO: Falloff
-
+    vec3f light_dir;
+    rtfloat light_dist;
+    if (light.type == light_type::directional) {
+      light_dir = -light.dir;
+      light_dist = rtfloat_inf;
+    } else if (light.type == light_type::point) {
+      light_dir = normalize(light.pos - point);
+      light_dist = magnitude(light.pos - point);
+    }
+    
     /* Ambient shading */
     result = result + ka * light.color;
 
+    /* Shadow test */
+    if (light.type != light_type::ambient) {
+      // TODO: Figure out the proper way to deal with this constant
+      ray3f ray = { point + 0.001 * light_dir, light_dir };
+      intersection hit = trace_ray(s, ray);
+      if (hit.dist < light_dist)
+        continue;
+    }
+
     /* Diffuse shading */
     if (light.type != light_type::ambient) {
-      vec3f light_dir;
-      if (light.type == light_type::directional)
-        light_dir = -light.dir;
-      else
-        light_dir = normalize(light.pos - point);
-
       rtfloat diff_factor = std::max(dot(light_dir, normal), 0.0);
       result = result + diff_factor * kd * light.color;
     }
-
-    /* Specular shading */
-    // TODO: Specular shading
   }
 
   return result;
 }
 
 
-static color3f trace_ray(scene *s, ray3f ray) {
-  rtfloat min_t = rtfloat_inf;
-  scene_object *hit_obj = nullptr;
+static color3f trace_color(scene *s, ray3f ray) {
+  intersection hit = trace_ray(s, ray);
 
-  for (scene_object *obj : s->objects) {
-    // TODO: Transformation computations
-    rtfloat t = obj->ray_test(ray);
-    if (t < min_t) {
-      min_t = t;
-      hit_obj = obj;
-    }
-  }
+  if (hit.obj == nullptr) return {0, 0, 0};
 
-  if (hit_obj == nullptr) return {0, 0, 0};
-
-  vec3f point = ray.start + min_t * ray.dir;
-  return compute_shading(s, hit_obj, point, s->camera.eye);
+  vec3f point = ray.start + hit.dist * ray.dir;
+  return compute_shading(s, hit.obj, point, s->camera.eye);
 }
 
 
@@ -77,7 +94,7 @@ void scene_render(scene *s, size_t width, size_t height,
       vec3f p  = (1 - v)*p0 + v*p1;
 
       ray3f ray = { eye, p - eye };
-      write_pixel(trace_ray(s, ray));
+      write_pixel(trace_color(s, ray));
     }
   }
 }
