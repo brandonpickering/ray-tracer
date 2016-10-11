ef1ac4c844728d4ee95812bd4894bfc41dda728a
Brandon Pickering <brandonpickering95@gmail.com>
2016-10-11 11:29:31 -0700

    Add image size command argument

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <string>
 
 #include "common.hpp"
 #include "image.hpp"
@@ -6,13 +7,49 @@
 #include "shapes.hpp"
 
 
-int main() {
+static size_t img_width = 700, img_height = 700;
+
+
+static void read_arguments(scene *s, int argc, char *argv[]) {
+  for (int i = 0; i < argc; i++) {
+    std::string arg = argv[i];
+
+    if (arg == "--size" || arg == "-s") {
+      if (i + 2 >= argc) {
+        fprintf(stderr, "Error: Expected two numbers after --size flag\n");
+        exit(1);
+      }
+      std::string w = argv[++i];
+      std::string h = argv[++i];
+
+      const char *endptr = w.c_str();
+      int width = (int) strtol(w.c_str(), (char **) &endptr, 10);
+      if (endptr != w.c_str() + w.size() || width < 0) {
+        fprintf(stderr, "Error: Invalid first argument to --size\n");
+        exit(1);
+      }
+
+      endptr = h.c_str();
+      int height = (int) strtol(h.c_str(), (char **) &endptr, 10);
+      if (endptr != h.c_str() + h.size() || height < 0) {
+        fprintf(stderr, "Error: Invalid second argument to --size\n");
+        exit(1);
+      }
+
+      img_width = (size_t) width;
+      img_height = (size_t) height;
+    }
+  }
+}
+
+
+int main(int argc, char *argv[]) {
   scene *s = scene_create(stdin);
   if (s == nullptr) exit(1);
+  read_arguments(s, argc, argv);
 
-  size_t w = 640, h = 640;
-  image_output_stream write_pixel = ppm_stream(stdout, w, h);
-  scene_render(s, w, h, write_pixel);
+  image_output_stream write_pixel = ppm_stream(stdout, img_width, img_height);
+  scene_render(s, img_width, img_height, write_pixel);
 
   scene_destroy(s);
   return 0;
